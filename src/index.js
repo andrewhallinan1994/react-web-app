2e3dcd0cf42336969cb4574b397138798e856b19
andrewhallinan1994 <52375134+andrewhallinan1994@users.noreply.github.com>
2020-04-13 16:02:57 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
-
-//import * as serviceWorker from './serviceWorker';
-
 
 const message = (<p> Hello world! </p>);
 
-ReactDOM.render(
-  /*<React.StrictMode>
-    <App />
-  </React.StrictMode>*/
-  message,
-  document.getElementById('root')
-);
-
+ReactDOM.render(message, document.getElementById('root') );
 
